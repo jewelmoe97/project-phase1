76ad24f30498990c22ea724ca2a5fe21ceed234c
jewelmoe97 <68708848+jewelmoe97@users.noreply.github.com>
2022-06-12 14:30:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,69 +1,143 @@
 
 
-window.addEventListener('load', () => {
-	const form = document.querySelector("#new-task-form");
-	const input = document.querySelector("#new-task-input");
-	const list_el = document.querySelector("#tasks");
-	//function to store items in local storage
-	function storeTasks(task){
-		task.push(task);
-		localStorage.storeTasks('task',JSON.stringify(task));
-	  };
-//  to add new task
-	form.addEventListener('submit', (e) => {
-		e.preventDefault();
 
-		const task = input.value;
 
-		const task_el = document.createElement('div');
-		task_el.classList.add('task');
 
-		const task_content_el = document.createElement('div');
-		task_content_el.classList.add('content');
+let change;
 
-		task_el.appendChild(task_content_el);
+let table = document.querySelector("table");
 
-		const task_input_el = document.createElement('input');
-		task_input_el.classList.add('text');
-		task_input_el.type = 'text';
-		task_input_el.value = task;
-		task_input_el.setAttribute('readonly', 'readonly');
+let body = document.body;
 
-		task_content_el.appendChild(task_input_el);
+let id = document.querySelector(".id");
 
-		const task_actions_el = document.createElement('div');
-		task_actions_el.classList.add('actions');
-// to edit the task	
-		const task_edit_el = document.createElement('button');
-		task_edit_el.classList.add('edit');
-		task_edit_el.innerText = 'Edit';
-// to delete the task
-		const task_delete_el = document.createElement('button');
-		task_delete_el.classList.add('delete');
-		task_delete_el.innerText = 'Delete';
+let todo = document.querySelector(".todo");
 
-		task_actions_el.appendChild(task_edit_el);
-		task_actions_el.appendChild(task_delete_el);
+let done = document.querySelector(".done");
 
-		task_el.appendChild(task_actions_el);
+function getdata() {
+  const Url = "http://localhost:3000/list";
 
-		list_el.appendChild(task_el);
+  fetch(Url, {
+    method: "GET",
+  })
+    .then((response) => response.text())
 
-		input.value = '';
+    .then((result) => {
+      change = JSON.parse(result);
+      console.log(change);
 
-		task_edit_el.addEventListener('click', (e) => {
-			if (task_edit_el.innerText.toLowerCase() == "edit") {
-				task_edit_el.innerText = "Save";
-				task_input_el.removeAttribute("readonly");
-				task_input_el.focus();
-			} else {
-				task_edit_el.innerText = "Edit";
-				task_input_el.setAttribute("readonly", "readonly");
-			}
-		});
+      for (let i = 0; i < change.length; i++) {
+        let i_view = change[i].id;
 
-		task_delete_el.addEventListener('click', (e) => {
-			list_el.removeChild(task_el);
-		});
-	});
+        var main = document.createElement("tr");
+
+        body.append(main);
+
+        table.append(main);
+
+        let d1 = document.createElement("td");
+        let d2 = document.createElement("td");
+       
+        let d4 = document.createElement("button");
+        let d5 = document.createElement("td");
+
+        let d6 = document.createElement('button');
+
+        d5.append(d6);
+        
+        d6.textContent='Done';
+        d6.classList.add('done');
+
+        d1.textContent = change[i].id;
+        d2.textContent = change[i].todo;
+       
+        d4.textContent = "Delete";
+        d4.classList.add("delete");
+
+        main.append(d1);
+
+        main.append(d2);
+
+        
+
+        d5.append(d4);
+
+        main.append(d5);
+
+
+        d6.addEventListener("click" , function(){
+			d2.classList.toggle('underline');
+		})
+           
+        
+
+        d4.onclick = function () {
+          fetch(`http://localhost:3000/list/${change[i].id}`, {
+            method: "DELETE",
+            headers: {
+              "Content-type": "application/json; charset=UTF-8",
+            },
+          })
+            .then((response) => {
+              return response.json();
+            })
+            .then((data) => {
+              console.log(data);
+              window.location.href = "index.html";
+            });
+        };
+      }
+    });
+}
+
+getdata();
+
+const addbtn = document.querySelector(".add");
+
+let ip = document.querySelector(".ip");
+
+addbtn.addEventListener("click", add);
+
+function add() {
+  if (ip.value == "") {
+    alert("Please input list ");
+    ip.value = "";
+    ip.focus();
+  } else {
+    function loaddata() {
+      const Url = " http://localhost:3000/list";
+
+      let data = {
+        todo: ip.value,
+        
+      };
+
+      fetch(Url, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.text())
+
+        .then((data) => {
+          console.log("Success", data);
+          window.location.href = "index.html";
+        })
+
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    loaddata();
+  }
+}
+
+window.addEventListener("keydown", function keydw(e) {
+  if (e.key == "Enter") {
+    return add();
+  }
 });
