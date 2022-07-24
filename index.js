addd4cec2827442e96e7ee68b3f53c42eafdc878
jewel <snowdecember079@gmail.com>
2022-07-24 00:41:14 -0400

    pass

--- index.js
+++ index.js
@@ -1,111 +1,128 @@
-
-
-
-
-
-let change;
-
-let table = document.querySelector("table");
-
-let body = document.body;
-
-let id = document.querySelector(".id");
-
-let todo = document.querySelector(".todo");
-
-let done = document.querySelector(".done");
-
-function getdata() {
-  const Url = "http://localhost:3000/list";
-
-  fetch(Url, {
-    method: "GET",
-  })
-    .then((response) => response.text())
-
-    .then((result) => {
-      change = JSON.parse(result);
-      console.log(change);
-
-      for (let i = 0; i < change.length; i++) {
-        let i_view = change[i].id;
-
-        var main = document.createElement("tr");
-
-        body.append(main);
-
-        table.append(main);
-
-        let d1 = document.createElement("td");
-        let d2 = document.createElement("td");
-       
-        let d4 = document.createElement("button");
-        let d5 = document.createElement("td");
-
-        let d6 = document.createElement('button');
-
-        d5.append(d6);
-        
-        d6.textContent='Done';
-        d6.classList.add('done');
-
-        d1.textContent = change[i].id;
-        d2.textContent = change[i].todo;
-       
-        d4.textContent = "Delete";
-        d4.classList.add("delete");
-
-        main.append(d1);
-
-        main.append(d2);
-
-        
-
-        d5.append(d4);
-
-        main.append(d5);
-
-
-        d6.addEventListener("click" , function(){
-			d2.classList.toggle('underline');
-		})
-           
-        
-
-        d4.onclick = function () {
-          fetch(`http://localhost:3000/list/${change[i].id}`, {
-            method: "DELETE",
-            headers: {
-              "Content-type": "application/json; charset=UTF-8",
-            },
-          })
-            .then((response) => {
-              return response.json();
-            })
-            .then((data) => {
-              console.log(data);
-              window.location.href = "index.html";
-            });
-        };
-      }
-    });
-}
-
-getdata();
-
-const addbtn = document.querySelector(".add");
+show_data();
 
 let ip = document.querySelector(".ip");
 
+let table = document.querySelector('table');
+
+const addbtn = document.querySelector(".add");
+
 addbtn.addEventListener("click", add);
 
-function add() {
+
+async function show_data(){
+
+
+  const Url = "http://localhost:3000/list";
+   
+    await fetch(Url, {
+      method: "GET",
+    })
+      .then((response) => response.text())
+  
+      .then((result) => {
+          
+        if(result == null){
+          task = [];
+        }
+
+        else{
+          task = JSON.parse(result);
+        }
+        
+        cb(task);
+
+         
+      })
+  
+      .catch( (err) =>{
+        console.log(err);
+      } )
+ 
+
+  
+}
+
+function cb(res){
+  let table = document.querySelector('table');
+
+  let results = '';
+    
+   results += '<tr> <th>No.</th> <th>I wanna...</th> <th>Action</th>'
+  
+  res.forEach( (a,index)=>{
+       
+   results += `<tr> <td> ${a.id} </td>
+      <td class='t3'>  ${a.todo} </td>
+      <td>  <button class='done' onclick='done(${index})'> Done  </button> 
+        <button class='delete' onclick='del(${a.id})'> Delete </button>     </td>  
+     </tr>`
+
+    
+  })
+
+  table.innerHTML = results;
+
+  results += '</tr>'
+
+ 
+}
+
+
+async function del(del_id){
+      
+  await fetch(`http://localhost:3000/list/${del_id}`, {
+    method: "DELETE",
+    headers: {
+        "Content-type": "application/json; charset=UTF-8",
+    },
+})
+
+.then( (response) =>{
+ return response.text();
+
+})
+
+.then( (ress) =>{
+  console.log(ress);
+  return show_data();
+})
+
+.catch( (err) =>{
+  console.log(err);
+} )
+
+
+
+}
+
+
+function done(id){
+
+ 
+  let t3 = document.querySelectorAll('.t3');
+   
+
+  t3[id].classList.toggle('underline');
+ 
+}
+
+
+
+function add(e){
+  e.preventDefault();
+
   if (ip.value == "") {
     alert("Please input list ");
     ip.value = "";
     ip.focus();
-  } else {
-    function loaddata() {
+
+  }
+
+
+  else{
+
+   async function loaddata(){
+
       const Url = " http://localhost:3000/list";
 
       let data = {
@@ -113,7 +130,7 @@ function add() {
         
       };
 
-      fetch(Url, {
+     await fetch(Url, {
         method: "POST",
         body: JSON.stringify(data),
         headers: {
@@ -124,20 +141,59 @@ function add() {
 
         .then((data) => {
           console.log("Success", data);
-          window.location.href = "index.html";
+          ip.value = '';
+          ip.focus();
+          show_data();
+         
+       
         })
 
         .catch((error) => {
           console.log(error);
         });
     }
-
     loaddata();
-  }
+    }  
+
 }
+
+   let black = true;
+
+  
+   
+ let dark = document.querySelector('.dark');
+
+ 
+ if(black == true){
+  dark.textContent = 'Background Change'
+  
+ }
+
+ 
+
+ let body = document.body;
+
+ let red = document.querySelector('.red');
+
+
+ dark.addEventListener('click', function(){
+  console.log('change to light mode');
+  body.classList.toggle('light');
+  
+  table.classList.toggle('lb');
+ })
+
+
 
 window.addEventListener("keydown", function keydw(e) {
   if (e.key == "Enter") {
-    return add();
+    return  add(e);
   }
 });
+
+
+
+
+
+
+
